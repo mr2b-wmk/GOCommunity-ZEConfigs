9a6110ff16c1afd7759e7c133bccea3ada1979f9
Moefox <47162655+kimoefox@users.noreply.github.com>
2023-08-04 00:07:20 +0800

    update

--- console-mannager/main.py
+++ console-mannager/main.py
@@ -10,6 +10,8 @@
 import re
 import os
 import shutil
+import subprocess
+import requests
 
 header = "\t\""
 Tail = "\"\n\t{\n\t\t\t\"default\"\t\t\"\"\n\t\t\t\"timer\"\t\t\"0\"\n\t\t\t\"timercover\"\t\t\"0\"\n\t\t\t\"timertip\"\t\t\"倒计时 {time} 秒\"\n\t\t\t\"timerend\"\t\t\"\"\n\t}\n"
@@ -17,9 +19,48 @@ top_header = "\"ConsoleMessage\"\n{\n"
 top_tail = "\n}"
 
 ###########
-###函数区##
+###函数区###
 ###########
 
+##检查是否拥有反编译工具，没有就下载
+def check_and_download_file(url, filename):
+    if os.path.exists(filename):
+        print("反编译工具存在，正在开始工作")
+    else:
+        print("反编译工具不存在，正在开始下载")
+        try:
+            response = requests.get(url)
+            with open(filename, "wb") as file:
+                file.write(response.content)
+            print(f"下载 '{filename}' 完成.继续进行地图检测文本")
+        except requests.RequestException as e:
+            print(f" 尝试从 '{url}'下载 '{filename}'失败，错误为: {e}")
+            input("请按下任意键退出程序...")
+            exit()
+
+##检查是否有且只有一个bsp文件
+def count_bsp_files():
+    bsp_files = [file for file in os.listdir() if file.endswith(".bsp")]
+    return len(bsp_files)
+
+##检查是否没有vmf文件
+def count_vmf_files():
+    vmf_files = [file for file in os.listdir() if file.endswith(".vmf")]
+    return len(vmf_files)
+
+
+##寻找bsp并记录进目录中
+def find_bsp_files(directory):
+    bsp_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".bsp"):
+                bsp_files.append(os.path.abspath(os.path.join(root, file)))
+    return bsp_files
+def save_to_file(file_list, output_file):
+    with open(output_file, "w") as f:
+        for file_path in file_list:
+            f.write(file_path + "\n")
 
 ##将vmf复制一份并重命名，不对原文件进行操作
 def copy_and_rename_files(source_folder, destination_folder, target_extension, new_name):
@@ -106,7 +147,7 @@ def modify_file(file_path):
         print(f"An error occurred: {e}")
 ##批量删除过程垃圾模块
 def delete_files():
-    files_to_delete = ["1command.txt", "2text.txt", "3text_deduplicated.txt", "4text_formatted.cfg", "input.txt"]
+    files_to_delete = ["1command.txt", "2text.txt", "3text_deduplicated.txt", "4text_formatted.cfg", "input.txt", "path.txt"]
     current_directory = os.getcwd()
 
     for filename in files_to_delete:
@@ -119,6 +160,54 @@ def delete_files():
 ###########
 ###操作区###
 ###########
+
+##主函数前检查
+if __name__ == "__main__":
+    jar_file_url = "https://gitee.com/moefox_kimo/disk/releases/download/test/bspsrc.jar"
+    jar_file_name = "bspsrc.jar"
+    print("正在检查bsp文件数量是否唯一")
+    bsp_count = count_bsp_files()
+    if bsp_count == 1:
+        print("只有一个bsp文件，检查vmf数量中")
+    elif bsp_count > 1:
+        print("多个bsp文件")
+        input("请按下任意键退出程序...")
+        exit()
+    else:
+        print("没有找到bsp文件")
+        input("请按下任意键退出程序...")
+        exit()
+    vmf_count = count_vmf_files()
+    if vmf_count == 0:
+        print("vmf文件不存在，正在检查是否包含反编译工具")
+    else:
+        print("存在vmf文件")
+        input("请按下任意键退出程序...")
+    check_and_download_file(jar_file_url, jar_file_name)
+    # 继续执行后面的代码
+    print("正在执行其他命令")
+
+
+if __name__ == "__main__":
+    target_directory = "."  # 可以修改为你想要查找的目录
+    output_file = "path.txt"
+
+    bsp_files = find_bsp_files(target_directory)
+    save_to_file(bsp_files, output_file)
+    print(f"Found {len(bsp_files)} .bsp files. Paths saved in {output_file}.")
+    print(os.path.dirname(os.path.abspath(__file__)))
+
+# 要执行的命令
+path1 = os.path.dirname(os.path.abspath(__file__))
+command = "java -jar .\\bspsrc.jar -l " + path1 + "\path.txt"  # 这里使用了Linux的ls命令，可以替换成其他命令或程序
+
+# 使用subprocess.run()函数执行命令
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+# 输出命令的执行结果
+print("返回值:", result.returncode)
+print("标准输出:", result.stdout)
+print("错误输出:", result.stderr)
 
 ##重命名
 if __name__ == "__main__":
